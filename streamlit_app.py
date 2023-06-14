6eafa2056e66456ac1bf27156de0e6154d290cf2
gohilkush <112783124+gohilkush@users.noreply.github.com>
2023-06-14 13:53:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,6 +54,7 @@ streamlit.header('View our Fruit List - Add Your Favorites!')
 
 if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  use role accountadmin;
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
@@ -68,6 +69,7 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  use role accountadmin;
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
