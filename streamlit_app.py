c1307976b2060c6643161ce05f00e17665871c3f
gohilkush <112783124+gohilkush@users.noreply.github.com>
2023-06-14 13:54:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,7 @@ except URLError as e:
 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
+    my_cur.execute("use role accountadmin")
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
@@ -54,7 +55,6 @@ streamlit.header('View our Fruit List - Add Your Favorites!')
 
 if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  use role accountadmin;
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
@@ -63,13 +63,13 @@ if streamlit.button('Get Fruit List'):
 
 def insert_row_snowflake(new_fruit):
   with my_cnx_cursor() as my_cur:
+    my_cur.execute("use role accountadmin")
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding my " + new_fruit
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  use role accountadmin;
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
